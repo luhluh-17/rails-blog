91c908d280c762294eebe65c58c038f06536defb
luhluh-17 <castro.ralph17@gmail.com>
2022-09-13 07:52:33 +0800

    added method to return a format readable date

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,8 @@
 module ArticlesHelper
+  # Method that accepts a date object as an argument and
+  # returns a human readable format
+  # ex. January 1, 2022
+  def format_date(date)
+    date.strftime('%B %d, %Y')
+  end
 end
