09875c8e287e350ad48e22a17edcdf39446dbdac
luhluh-17 <castro.ralph17@gmail.com>
2022-09-13 13:50:35 +0800

    delete scaffold

--- app/helpers/articles_helper.rb
+++
@@ -1,8 +0,0 @@
-module ArticlesHelper
-  # Method that accepts a date object as an argument and
-  # returns a human readable format
-  # ex. January 1, 2022
-  def format_date(date)
-    date.strftime('%B %d, %Y')
-  end
-end
