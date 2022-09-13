d4b6f1bba424aaf28ee7210ff4fd49ce677d4fac
luhluh-17 <castro.ralph17@gmail.com>
2022-09-13 18:13:18 +0800

    add initial styling

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,13 @@
 module ArticlesHelper
+  def readable_date_formatter(date)
+    date.strftime('%B %d, %Y')
+  end
+
+  def trim_content(content)
+    content.length <= 500 ? content : "#{content[0..500]}..."
+  end
+
+  def words_to_time(content)
+    "#{content.length / 100} min read"
+  end
 end
