45a0a293673f2c295cfcc12a0e6cf7772c6274d2
luhluh-17 <castro.ralph17@gmail.com>
2022-09-13 18:58:57 +0800

    add a method to set temporary image for empty url

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,4 +1,6 @@
 module ArticlesHelper
+  TEMP_IMAGE = 'https://img.freepik.com/free-vector/error-404-concept-landing-page_52683-10996.jpg?w=996&t=st=1663066300~exp=1663066900~hmac=958711f7fd9f902aca3827891aeb30d344c71b4eac1ae8f398c8197771d06774'.freeze
+
   def readable_date_formatter(date)
     date.strftime('%B %d, %Y')
   end
@@ -9,5 +11,9 @@ module ArticlesHelper
 
   def words_to_time(content)
     "#{content.length / 100} min read"
+  end
+
+  def validate_image(url)
+    url.empty? ? TEMP_IMAGE : url
   end
 end
