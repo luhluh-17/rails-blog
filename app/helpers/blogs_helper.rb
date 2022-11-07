ec080e1eae099417f7fc4591833d3713265a9734
luhluh-17 <castro.ralph17@gmail.com>
2022-11-08 04:06:07 +0800

    add helper functions to blog

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,17 @@
 module BlogsHelper
+  def custom_distance_of_time(date)
+    if Time.now < date + 1.days
+      "#{distance_of_time_in_words(date, Time.now)} ago"
+    else
+      readable_date_formatter(date)
+    end
+  end
+
+  def readable_date_formatter(date)
+    date.strftime('%B %d, %Y')
+  end
+
+  def words_to_time(content)
+    "#{content.length / 100} min read"
+  end
 end
