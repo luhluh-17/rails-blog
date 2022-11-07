194854b2e807ac08b3fdbf0b3df19378dc70e92c
luhluh-17 <castro.ralph17@gmail.com>
2022-11-08 06:21:22 +0800

    add model validation

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,14 @@
 class Blog < ApplicationRecord
+  validates :title,
+            presence: true,
+            length: { maximum: 50,
+                      too_long: '200 characters is the maximum allowed' }
+  validates :summary,
+            presence: true,
+            length: { maximum: 200,
+                      too_long: '200 characters is the maximum allowed' }
+  validates :content,
+            presence: true,
+            length: { maximum: 1000,
+                      too_long: '1000 characters is the maximum allowed' }
 end
