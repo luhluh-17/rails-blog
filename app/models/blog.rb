bb5c2618ff9a59165fd496eebe0ef9c8e6900e42
luhluh-17 <castro.ralph17@gmail.com>
2022-11-08 06:35:39 +0800

    update model entity length

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,14 +1,14 @@
 class Blog < ApplicationRecord
   validates :title,
             presence: true,
-            length: { maximum: 50,
-                      too_long: '200 characters is the maximum allowed' }
+            length: { maximum: 200,
+                      too_long: '50 characters is the maximum allowed' }
   validates :summary,
             presence: true,
-            length: { maximum: 200,
-                      too_long: '200 characters is the maximum allowed' }
+            length: { maximum: 5000,
+                      too_long: '300 characters is the maximum allowed' }
   validates :content,
             presence: true,
-            length: { maximum: 1000,
+            length: { maximum: 10_000,
                       too_long: '1000 characters is the maximum allowed' }
 end
