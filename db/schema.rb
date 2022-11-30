ccfbfba85a7d70697ab2a3de64dc8a3d0f1c55e7
luhluh-17 <castro.ralph17@gmail.com>
2022-12-01 01:34:56 +0800

    deploy in railway

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_11_06_214824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
